59209541b5e291f29230e89c31bf9e56ed894e08
Cameron Wilson <38517335+ItzCam@users.noreply.github.com>
2020-10-13 10:15:42 -0700

    Update MiiTunesPlayer.java

--- Semester Project/src/MiiTunes/MiiTunesPlayer.java
+++ Semester Project/src/MiiTunes/MiiTunesPlayer.java
@@ -15,7 +15,7 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 * @author Antonio Hughes 
 * @author Noah Avina
 * 
-* Version 1.1
+* Version 1.0 
 * 
 * This class when ran will display our new GUI box
 */
