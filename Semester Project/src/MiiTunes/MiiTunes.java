4207d3973ef8378a1ec8554efa1359f042b01e7a
Cameron Wilson <38517335+ItzCam@users.noreply.github.com>
2020-10-13 10:10:32 -0700

    Update MiiTunes.java

--- Semester Project/src/MiiTunes/MiiTunes.java
+++ Semester Project/src/MiiTunes/MiiTunes.java
@@ -1,10 +1,21 @@
 package MiiTunes;
 
+/**
+ * Main class to run MiiTunes MP3 program
+ * 
+ * @author Antonio Hughes
+ * @author Noah Avina
+ * 
+ * Version 1.0 - Initial commit on Github 
+ *
+ */
+
 public class MiiTunes {
 
     static View view;
 
     public static void main(String[] args) {
+    	
         view = new View();
     }
 } 
