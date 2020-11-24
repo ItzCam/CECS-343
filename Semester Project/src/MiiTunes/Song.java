b8489cec6c4c3b9d7684dbfb59a9b84645a62a59
Cameron Wilson <38517335+ItzCam@users.noreply.github.com>
2020-11-23 18:18:15 -0800

    Update Song.java
    Use song paths for play instead of song object

--- Semester Project/src/MiiTunes/Song.java
+++ Semester Project/src/MiiTunes/Song.java
@@ -1,15 +1,24 @@
 package MiiTunes;
 
+import java.io.File;
 import java.io.IOException;
 
 import com.mpatric.mp3agic.ID3v1;
-
 import com.mpatric.mp3agic.ID3v24Tag;
 import com.mpatric.mp3agic.Mp3File;
 import com.mpatric.mp3agic.InvalidDataException;
 import com.mpatric.mp3agic.NotSupportedException;
 import com.mpatric.mp3agic.UnsupportedTagException;
-import java.io.File;
+
+/**
+ * Song class represents the song object in the MiiTunes MP3 player
+ * 
+ * @author Antonio Hughes
+ * @author Noah Avina
+ * 
+ * Version 1.1 - Use song paths for play instead of song object
+ *
+ */
 
 public class Song {
     private Mp3File file;
@@ -41,6 +50,9 @@ public class Song {
         }
     }
 
+    /**
+     * This method checks if tags of mp3 files are not null or invalid values
+     */
     public void checkTags() {
         boolean changesMade = false;
         if(tag.getTitle() == "" || tag.getTitle() == null) {
@@ -71,10 +83,12 @@ public class Song {
         if(changesMade) file.setId3v1Tag(tag);
     }
 
+    // Method Gets the name of the file based on its path
     public String getFilename() {
         return filename;
     }
 
+    // Method Saves the songs path
     public void saveSong(String path) {
         try {
             file.save(path);
@@ -84,6 +98,7 @@ public class Song {
     }
 
     // Method returns total frames of mp3 file
+    
     public long getFrames() {
         return file.getFrameCount();
     }
@@ -94,7 +109,7 @@ public class Song {
     }
 
     // Method returns total bytes of mp3 file
-    public Long getBytes() {
+    public long getBytes() {
         return file.getLength();
     }
 
@@ -118,43 +133,52 @@ public class Song {
         return tag.getAlbum();
     }
 
+    // Method returns year song was released
     public String getYear() {
         return tag.getYear();
     }
 
+    // Method returns the genre
     public int getGenre() {
         return tag.getGenre();
     }
 
+    // Method returns any comments
     public String getComment() {
         return tag.getComment();
     }
 
+    // Set the title of the song 
     public void setTitle(String title) {
         tag.setTitle(title);
         file.setId3v1Tag(tag);
     }
 
+    // Set the artist of the song
     public void setArtist(String artist) {
         tag.setArtist(artist);
         file.setId3v1Tag(tag);
     }
 
+    // Set the album name 
     public void setAlbum(String album) {
         tag.setAlbum(album);
         file.setId3v1Tag(tag);
     }
 
+    // Set the year it was released
     public void setYear(String year) {
         tag.setYear(year);
         file.setId3v1Tag(tag);
     }
 
+    // Set the genre of the song
     public void setGenre(int genre) {
         tag.setGenre(genre);
         file.setId3v1Tag(tag);
     }
 
+    // Set any comments for the song
     public void setComment(String comment) {
         tag.setComment(comment);
         file.setId3v1Tag(tag);
